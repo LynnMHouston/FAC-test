4a4e7c16a3915457bd16d2fcb7e80b94a0a653bb
LynnMHouston <lynn.houston@gsa.gov>
2025-03-11 09:08:07 -0400

    Update api_versions.py

--- backend/dissemination/api_versions.py
+++ backend/dissemination/api_versions.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 
 # These are API versions we want live.
 live = {
-    "dissemination": ["api_v1_0_3", "api_v1_1_0", TEST],
+    "dissemination": ["api_v1_0_3", "api_v1_1_0"],
     "support": ["admin_api_v1_1_0"],
 }
 
